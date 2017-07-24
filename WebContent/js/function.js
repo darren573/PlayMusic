c8bcfee3e8b3141ed55f7a825c1efe357f438aa4
darren573 <1415754221@qq.com>
2017-07-24 21:30:32 +0800

    简单的网页版播放器

---
+++ WebContent/js/function.js
@@ -0,0 +1,51 @@
+//页面加载的时候获得音频对象
+	 var a=null;
+$(function(){//主方法，所有方法逻辑执行完成之后再执行
+	a=document.getElementById("audio");
+	//添加播放的监听时间
+	a.addEventListener("timeupdate",timeupdate);
+	//点击播放按钮的事件
+	$("#pp").click(function(){
+		//获得播放状态
+		var flag = $(this).attr("flag");
+		if(flag=='0'){
+			a.play();
+			//获得播放的图片对象
+			$(this).find("img").attr("src","images/play.jpg");
+			//把flag设置成1
+			$(this).attr("flag","1");
+			//获得当前播放了多少秒，一共多少秒
+			
+			
+		}else{
+			//获得播放的图片对象
+			$(this).find("img").attr("src","images/pro.png");
+			//把flag设置成0
+			$(this).attr("flag","0");
+			a.pause();
+		}
+		
+	})
+})
+function timeupdate(){
+	//获得当前播放时间
+	var ct = a.currentTime;
+	//把秒变成分钟
+	var cmi=Math.floor(ct/60);
+	var cc=Math.floor(ct%60);
+	//把秒变成分钟
+	var du	= a.duration;
+	var dmi=Math.floor(du/60);
+	var dc=Math.floor(du%60);
+	$("#updatetime").html(cmi+":"+cc);
+	$("#totaltime").html(dmi+":"+dc);
+	//获得播放的长度
+	var left=Math.floor(ct/du*500)+"px";
+	$(".playball").css("left",left);
+	
+}
+function clickPro(e,obj){
+	var l=e.clientX-obj.offsetLeft;
+	$(".playball").css("left",l);
+	a.currentTime=(l/500)*a.duration;
+}
---
+++ WebContent/music.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Music Online</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="js/jquery-ui.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="js/iscroll.js"></script>
+<script type="text/javascript" src="js/function.js"></script>
+</head>
+<body>
+<audio id="audio" src="music/1.mp3"></audio>
+<div>
+<a id="pp" href="#" flag="0"><img height="50" src="images/pro.png"></a>
+</div>
+<br>
+<div id="updatetime" class="time"></div>
+<div class="pro" onclick="clickPro(event,this)">
+	<div class="playball"></div>
+</div>
+<div id="totaltime" class="time" style="text-align:right"></div>
+</body>
+</html>
